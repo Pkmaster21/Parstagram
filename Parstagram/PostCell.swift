9b3b1e037a42b43edbac6da9726e707bef564985
David Z <david.zhao.9991@gmail.com>
2021-10-19 22:52:31 -0400

    initial commit

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by David Zhao on 10/19/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
